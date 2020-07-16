4043ea28633d74c36cba94fd53211e711092fe68
oosor <3oosor@gmail.com>
2020-07-16 15:11:28 +0300

    patch repository

--- lib/Repositories/SharedModulesRepository.php
+++ lib/Repositories/SharedModulesRepository.php
@@ -9,9 +9,37 @@
 
 namespace Hedera\Repositories;
 
+use Doctrine\Common\Collections\Collection;
 use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+use GraphAware\Neo4j\OGM\Common\Collection as HederaCollection;
 
 class SharedModulesRepository extends BaseRepository
 {
+    /**
+     * @param Collection $collection
+     * @param string $classConfig
+     * @return Collection<array>
+     * @throws \Exception
+     */
+    public function loadConfigs(Collection $collection, string $classConfig)
+    {
+        if ($collection->isEmpty()) {
+            return new HederaCollection();
+        }
 
+        $ids = $collection->map(function ($item) {
+            return $item->getId();
+        });
+
+        $query = $this->entityManager
+            ->createQuery('MATCH (n:' . $this->getClassName() . ') WHERE ID(n) IN $id MATCH (n)<-[:MODULE_CONFIG_IN]-(conf) RETURN n, conf');
+        $query->setParameter('id', $ids);
+        $query
+            ->addEntityMapping('n', $this->getClassName())
+            ->addEntityMapping('conf', $classConfig);
+
+        $result = $query->execute();
+
+        return new HederaCollection($result);
+    }
 }

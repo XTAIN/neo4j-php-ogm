a3691548fc0bad7e930ab6e7948787331a47ea69
oosor <3oosor@gmail.com>
2020-07-17 13:59:34 +0300

    eager relation

--- lib/Repositories/Black/BlackRelationFieldsRepository.php
+++ lib/Repositories/Black/BlackRelationFieldsRepository.php
@@ -9,9 +9,45 @@
 
 namespace Hedera\Repositories\Black;
 
+use Doctrine\Common\Collections\Collection;
+use GraphAware\Neo4j\OGM\Common\Collection as HederaCollection;
 use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+use Hedera\Models\SharedIntermediaries;
 
 class BlackRelationFieldsRepository extends BaseRepository
 {
+    /**
+     * @param Collection $collection
+     * @return Collection<array>
+     * @throws \Exception
+     */
+    public function loadRelationsWithIntermediary(Collection $collection)
+    {
+        if ($collection->isEmpty()) {
+            return new HederaCollection();
+        }
 
+        $ids = $collection
+            ->map(
+                function ($item) {
+                    return $item->getId();
+                }
+            )
+            ->getValues();
+
+        $graph = last(explode('\\', $this->getClassName()));
+
+        $query = $this->entityManager
+            ->createQuery(
+                'MATCH (n:' . $graph . ') WHERE ID(n) IN $id MATCH (n)-[:INTERMEDIA_FIELD_IN]->(intermediary) RETURN n, intermediary'
+            );
+        $query->setParameter('id', $ids);
+        $query
+            ->addEntityMapping('n', $this->getClassName())
+            ->addEntityMapping('intermediary', SharedIntermediaries::class);
+
+        $result = $query->execute();
+
+        return new HederaCollection($result);
+    }
 }

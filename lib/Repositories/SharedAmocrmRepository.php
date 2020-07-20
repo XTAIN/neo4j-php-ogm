3c68db7c1479b25a77b0a96002397ed84927445a
oosor <3oosor@gmail.com>
2020-07-20 12:52:13 +0300

    upd rep

--- lib/Repositories/SharedAmocrmRepository.php
+++ lib/Repositories/SharedAmocrmRepository.php
@@ -9,9 +9,60 @@
 
 namespace Hedera\Repositories;
 
+use GraphAware\Neo4j\OGM\Common\Collection as HederaCollection;
 use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+use Hedera\Models\SharedAmocrm;
+use Hedera\Models\SharedIntegrations;
+use Hedera\Models\SharedOauth;
 
 class SharedAmocrmRepository extends BaseRepository
 {
+    /**
+     * @param SharedAmocrm $sharedAmocrm
+     * @param array $where ex.: [['oauth.code', '!=', 'tomato', 'OR'], ['integration.name', 'Teal']]
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function loadOAuthWithIntegration(SharedAmocrm $sharedAmocrm, array $where = [])
+    {
+        $graph = last(explode('\\', $this->getClassName()));
 
+        $cql = 'MATCH (n:' . $graph . ') WHERE ID(n) = $id';
+        $cql .= ' MATCH (n)<-[:AMOCRM_OAUTH_IN]-(oauth)-[:INTEGR_OAUTH_IN]->(integration)';
+
+        if (!empty($where)) {
+            $whereSql = '';
+            foreach ($where as $value) {
+                if (!is_array($value) || count($value) < 2) {
+                    continue;
+                }
+
+                $key = $value[0];
+                $operator = isset($value[2]) ? $value[1] : '=';
+                $val = $value[2] ?? $value[1];
+                $betweenOperator = $value[3] ?? 'AND';
+
+                if (empty($whereSql)) {
+                    $whereSql .= ' WHERE ';
+                } else {
+                    $whereSql .= " $betweenOperator ";
+                }
+
+                $whereSql .= "$key $operator '$val'";
+            }
+
+            $cql .= $whereSql;
+        }
+
+        $cql .= ' RETURN n, oauth, integration';
+
+        $query = $this->entityManager->createQuery($cql);
+        $query->setParameter('id', $sharedAmocrm->getId());
+
+        $query
+            ->addEntityMapping('n', $this->getClassName())
+            ->addEntityMapping('oauth', SharedOauth::class)
+            ->addEntityMapping('integration', SharedIntegrations::class);
+
+        return new HederaCollection($query->getResult());
+    }
 }

b0de59c7a71e60b4d646235076bc5d234734ffc7
Andrew <3oosor@gmail.com>
2020-09-16 12:58:47 +0300

    tomato observer repository upd

--- lib/Repositories/Tomato/TomatoObserverConfigsRepository.php
+++ lib/Repositories/Tomato/TomatoObserverConfigsRepository.php
@@ -9,9 +9,34 @@
 
 namespace Hedera\Repositories\Tomato;
 
+use GraphAware\Neo4j\OGM\Common\Collection as HederaCollection;
 use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+use Hedera\Models\SharedCustomersServices;
+use Hedera\Models\Tomato\TomatoTrackingInternetDocuments;
 
 class TomatoObserverConfigsRepository extends BaseRepository
 {
+    /**
+     * @param SharedCustomersServices $sharedCustomersServices
+     * @return HederaCollection
+     */
+    public function loadObserverConfigsWithTrackingIDForSCS(SharedCustomersServices $sharedCustomersServices)
+    {
+        $scsGraph = last(explode('\\', SharedCustomersServices::class));
+        $trackGraph = last(explode('\\', TomatoTrackingInternetDocuments::class));
+        $graph = last(explode('\\', $this->getClassName()));
 
+        $cql = 'MATCH (n:' . $scsGraph . ')<-[:TOMATO_SERVICE_IN]-(observer:' . $graph . ')-[:TOMATO_TRACK_ID_IN]->(track:' . $trackGraph . ')';
+        $cql .= ' WHERE ID(n) = $id';
+        $cql .= ' RETURN observer, track';
+
+        $query = $this->entityManager->createQuery($cql);
+        $query->setParameter('id', $sharedCustomersServices->getId());
+
+        $query
+            ->addEntityMapping('observer', $this->getClassName())
+            ->addEntityMapping('track', TomatoTrackingInternetDocuments::class);
+
+        return new HederaCollection($query->getResult());
+    }
 }

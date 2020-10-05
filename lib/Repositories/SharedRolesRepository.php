e8ed571d7ce94d144681c30402dc3efcc56747ed
Andrew <3oosor@gmail.com>
2020-10-05 19:03:43 +0300

    upd roles repository

--- lib/Repositories/SharedRolesRepository.php
+++ lib/Repositories/SharedRolesRepository.php
@@ -9,9 +9,64 @@
 
 namespace Hedera\Repositories;
 
+use GraphAware\Neo4j\OGM\Common\Collection as HederaCollection;
 use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+use Hedera\Models\SharedScopes;
 
 class SharedRolesRepository extends BaseRepository
 {
+    /**
+     * @return HederaCollection $item [n => SharedRoles, scopes => HederaCollection<SharedScopes>]
+     */
+    public function loadRolesWithScopes()
+    {
+        $graph = last(explode('\\', $this->getClassName()));
 
+        $cqlRel = 'MATCH (n:' . $graph . ')-[:SHARED_ROLES_TO_SHARED_SCOPES]->(scopes) RETURN n, scopes';
+        $cqlWithoutRel = 'MATCH (n:' . $graph . ') WHERE NOT (n)-[:SHARED_ROLES_TO_SHARED_SCOPES]->() RETURN n';
+
+        $queryRel = $this->entityManager->createQuery($cqlRel);
+        $queryWithoutRel = $this->entityManager->createQuery($cqlWithoutRel);
+
+        $queryRel
+            ->addEntityMapping('n', $this->getClassName())
+            ->addEntityMapping('scopes', SharedScopes::class);
+
+        $queryWithoutRel
+            ->addEntityMapping('n', $this->getClassName());
+
+        $groupedRel = array_reduce(
+            $queryRel->getResult(),
+            function ($result, $item) {
+                $exist = head(
+                    array_filter(
+                        $result,
+                        function ($one) use ($item) {
+                            return $one['n']->getId() == $item['n']->getId();
+                        }
+                    )
+                );
+
+                if (empty($exist)) {
+                    $result[] = ['n' => $item['n'], 'scopes' => new HederaCollection([$item['scopes']])];
+                } else {
+                    $exist['scopes']->add($item['scopes']);
+                }
+
+                return $result;
+            },
+            []
+        );
+
+        $resultRel = new HederaCollection($groupedRel);
+        $resultWithoutRel = new HederaCollection($queryWithoutRel->getResult());
+
+        $resultWithoutRel = $resultWithoutRel->map(
+            function ($item) {
+                return ['n' => $item, 'scopes' => new HederaCollection()];
+            }
+        );
+
+        return new HederaCollection(array_merge($resultRel->getValues(), $resultWithoutRel->getValues()));
+    }
 }

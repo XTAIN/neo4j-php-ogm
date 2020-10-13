69b9dd0aa6ae58a53ba754da1f3f62bf390f0d05
Andrew <3oosor@gmail.com>
2020-10-13 18:51:42 +0300

    patch services repository

--- lib/Repositories/SharedCustomersServicesRepository.php
+++ lib/Repositories/SharedCustomersServicesRepository.php
@@ -10,8 +10,9 @@
 namespace Hedera\Repositories;
 
 use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+use Hedera\Helpers\WithBuilder;
 
 class SharedCustomersServicesRepository extends BaseRepository
 {
-
+    use WithBuilder;
 }

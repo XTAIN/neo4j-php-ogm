1dcabd3f858e9a8ad1fa573926d9f56c6e6241f3
sdanke <sdanke@xtain.net>
2022-11-02 16:37:17 +0100

    removed files

--- lib/Lara/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/status/{token}', 'GuardInfoController@index');
-Route::get('/clear-cache-smart/{token}', 'ClearCacheController@clearSmart');
